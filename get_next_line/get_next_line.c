d695aded865862e5539ccf46151e30375c121307
Manupoons <manumar2002@gmail.com>
2024-09-26 11:27:31 +0200

    little changes

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -12,7 +12,30 @@
 
 #include "get_next_line.h"
 
+
+/*
 char	*get_next_line(int fd)
 {
+	char	*buffer;
+	char	*line;
 
+	buffer = BUFFER_SIZE;
+	fd = open("a.txt", O_RDONLY);
+	read(fd, buffer, BUFFER_SIZE);
+	if (fd < 0)
+		return (NULL);
+	printf("Text:  %s\n", buffer);
+}
+*/
+
+int main()
+{
+	char	*buffer[42];
+	char	*line;
+
+	int fd = open("a.txt", O_RDONLY);
+	read(fd, buffer, BUFFER_SIZE);
+	if (fd < 0)
+		return (0);
+	printf("Text:  %s\n", *buffer);
 }
