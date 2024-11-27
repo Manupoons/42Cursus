f921a80a81bfc3962ce2d60adf2bc46e13046c51
Manupoons <manumar2002@gmail.com>
2024-11-27 11:45:10 +0100

    deleted atoi from client.c

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,36 +6,11 @@
 /*   By: mamaratr <mamaratr@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 12:03:59 by mamaratr          #+#    #+#             */
-/*   Updated: 2024/11/26 11:31:00 by mamaratr         ###   ########.fr       */
+/*   Updated: 2024/11/27 11:44:13 by mamaratr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-
-int	ft_atoi(const char *str)
-{
-	int		i;
-	int		sign;
-	int		num;
-
-	i = 0;
-	sign = 1;
-	num = 0;
-	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-		i++;
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			sign *= -1;
-		i++;
-	}
-	while (str[i] != '\0' && (str[i] >= '0' && str[i] <= '9'))
-	{
-		num = (num * 10) + (str[i] - '0');
-		i++;
-	}
-	return (num * sign);
-}
 
 void	convert_bin(char *s, int pid)
 {
