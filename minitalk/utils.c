bcf2986ed931d86b394ce4791f5f5bea174b8c8b
Manupoons <manumar2002@gmail.com>
2024-11-28 12:15:26 +0100

    modified atoi

--- minitalk/utils.c
+++ minitalk/utils.c
@@ -51,24 +51,14 @@ void	ft_putnbr(int num)
 int	ft_atoi(const char *str)
 {
 	int		i;
-	int		sign;
 	int		num;
 
 	i = 0;
-	sign = 1;
 	num = 0;
-	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-		i++;
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			sign *= -1;
-		i++;
-	}
 	while (str[i] != '\0' && (str[i] >= '0' && str[i] <= '9'))
 	{
 		num = (num * 10) + (str[i] - '0');
 		i++;
 	}
-	return (num * sign);
+	return (num);
 }
